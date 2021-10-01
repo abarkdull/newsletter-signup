f483c3c267120c10aae6a9bd91bf95e2af285fba
abarkdull <abarkdull@gmail.com>
2021-10-01 11:48:23 -0600

    add mailchimp api functionaliry

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -12,13 +13,51 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ] 
+    };
+
+    // flat pack JSON data into a string
+    var jsonData = JSON.stringify(data);
+
+    const url = 'https://us5.api.mailchimp.com/3.0/lists/86ad5e45fa'
+    const options = {
+        method: "POST",
+        auth: "austin1:22dd5364dadc4f9a7cf509dec1054334-us5"
+    };
+
+    const request = https.request(url, options, function(response) {
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
 });
+
+
+
 
 app.listen(3000, function() {
     console.log('server is running on port 3000');
 });
+
+// api key
+// 22dd5364dadc4f9a7cf509dec1054334-us5
+
+// list id 
+// 86ad5e45fa
